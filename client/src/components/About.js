f62f984e1b444569f835f7ad6394ab9f86f670b4
Amartya1802 <amartya21_ug@cse.nits.ac.in>
2022-10-22 03:18:40 +0530

    Navbar, main pagee updt2 xx

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,20 @@ import React from 'react'
 const About = () => {
   return (
     <div class="About">
+      <link rel="stylesheet" href="/styles/about.css"/>
       <p class="title"><h3>About Us</h3></p>
+      <br/>
+      <br/>
+      <div class="wrap">
+	      <div class="base">
+		      <div class="flowerpot"></div>
+		      <div class="blade blade-center"></div>
+		      <div class="blade blade-left-s"></div>
+		      <div class="blade blade-right-s"></div>
+		      <div class="blade blade-left-l"></div>
+		      <div class="blade blade-right-l"></div>
+	      </div>
+      </div>
     </div>
   )
 }
