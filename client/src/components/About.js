c46bd3491069693627857d7172b596b7f8afde91
Amartya1802 <amartya21_ug@cse.nits.ac.in>
2022-10-21 22:50:26 +0530

    Updating Nav, main, etc.

---
+++ client/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div class="About">
+      <p class="title"><h3>About Us</h3></p>
+    </div>
+  )
+}
+
+export default About
---
+++ client/src/components/Hero_Sec.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Hero_Sec = () => {
+  return (
+    <div class="part1">
+        <link rel="stylesheet" href="/styles/hero.css"/>
+      <div class="box-main">
+        <div class="firstHalf">
+          <p class="text-big0">EASE UP <br/> HOME FARMING</p>
+          {/* <p class="text-big">
+             
+            - INDIA -
+          </p> */}
+          <p class="text-small">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, rem. Neque ab, quae minima ea totam rerum eius 
+            ipsam architecto nihil voluptatum. Hic impedit qui adipisci aliquid iste, officia fuga.
+          </p>
+          {/* <div class="buttons">
+            <button class="btn2">Explore</button>
+          </div> */}
+          {/* <div class="icons"> */}
+            {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+            
+            <a href="#facebook" class="fa fa-facebook"></a>
+            <a href="#instgram" class="fa fa-instagram"></a>
+            <a href="#pinterest" class="fa fa-pinterest"></a>
+            <a href="#twitter" class="fa fa-twitter"></a>   
+            
+          </div> */}
+        </div>
+        <div class="secondHalf">
+            <image src="/img/pot.png" alt="Pot pic"></image>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero_Sec
