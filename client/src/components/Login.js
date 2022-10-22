cd19fc0a0e7b9a6166652cdd5a79322c7767f7d5
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2022-10-22 12:31:59 +0530

    adding login function

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,92 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {ImCross} from 'react-icons/im'
 
-const Login = () => {
+const Login = (props) => {
 
+    const {setLogged, setUser, password, setPassword, setFlash} = props
+  const [userTemp, setUserTemp] = useState('')
+  const [passTemp, setPassTemp] = useState('')
+  const host = 'https://craig-server.herokuapp.com'
     
+  const loginApi = async (endpoints,data_json, method) =>{
+    const response = await fetch(`${host}${endpoints}`, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data_json) // body data type must match "Content-Type" header
+    });
+    const json = await response.json();
 
+    if (json.status === true){
+      setFlash({msg:`Logged in as ${json.username}!`,category:'success'})
+      setUser(json.username)
+      setLogged(true)
+    }
+    else{
+      setFlash({msg:json.error,category:'danger'})
+      console.log(json.error);
+      setUser('default')
+    }
+
+    return json
+  }
+  
+  const signupApi = async (endpoints,data_json, method) =>{
+    const response = await fetch(`${host}${endpoints}`, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data_json) // body data type must match "Content-Type" header
+    });
+    const json = await response.json();
+    if (json.status === 'success'){
+      setFlash({msg:`Logged in as ${json.username}!`,category:'success'})
+      setUser(json.username)
+    }
+    else{
+      setFlash({msg:json.error,category:'danger'})
+      console.log(json.error);
+      setUser('default')
+    }
+      return json
+  }
+
+  const login = (username,pass) =>{
+    const data_json = {
+      username: username,
+      password: pass
+    }
+    const endpoints = '/api/auth/login'
+    loginApi(endpoints,data_json,'POST')
+  }
+
+  const signUp = (username) =>{
+    const data_json = {
+      username: username,
+      password: password
+    }
+    const endpoints = '/api/auth/createuser'
+    signupApi(endpoints,data_json,'POST')
+  }
   return (
-    <div>
-      
+    <>
+    <div className="backdrop"></div>
+    <div className='container login-section'>
+        <button className='close'><ImCross/></button>
+        <link rel="stylesheet" href="/styles/login.css" />
+        <img src="/img/login_illu.png" className="login-img" alt="" />
+        <div className="inpts">
+            <input type="text" value={userTemp} onChange={(e)=>{setUserTemp(e.target.value)}} name="User" id="user" placeholder='Enter your username..' />
+            <input type="password" value={passTemp} onChange={(e)=>{setPassTemp(e.target.value)}} name="password" id="password" placeholder='Enter your password..' />
+            <div className="inpts-btns d-flex my-2">
+              <button className='inpts-user' onClick={()=>{login(userTemp,passTemp)}} >Login</button>
+              <button className='inpts-user' onClick={()=>{setPassword(passTemp);signUp(userTemp)}} >SignUp</button>
+            </div>
+        </div>
     </div>
+    </>
   )
 }
 
